9e9386e7bec358d7f10e62815b1d1197a1c4927f
Dillon Smith <dillsmth@gmail.com>
2021-06-22 14:22:42 -0500

    initial commits

---
+++ index.js
@@ -0,0 +1,6 @@
+const generateHTML = require('./src/generateHTML');
+const fs = require('fs');
+
+const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
