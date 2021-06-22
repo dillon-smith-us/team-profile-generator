220695c97e2810abbe6edf6bc0cd3136399a5902
Dillon Smith <dillsmth@gmail.com>
2021-06-22 17:19:34 -0500

    adding utils

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const generateHTML = require('./src/generateHTML');
 const fs = require('fs');
 const inquirer = require('inquirer')
 const generatePage = Require('./src/page-template.js')
@@ -10,7 +9,7 @@ const questions = [
         name: 'role',
         message: 'what is the employee\'s role?',
         choices: () => {
-            if (allEmployees.some(employee => employee.role === 'Manager')) {
+            if (employees.some(employee => employee.role === 'Manager')) {
                 return['Engineer', 'Intern']
             } else {
                 return['Manager', 'Engineer', 'Intern']
@@ -42,5 +41,112 @@ const questions = [
                 return false;
             }
         }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: ({ firstName }) => `What is ${formatName(firstName)}'s id number?`,
+        valdate: idInput => {
+            if (!isNaN(parseInt(idInput))) {
+                return true;
+            } else {
+                console.log('Please enter a valid id number!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: ({ firstName }) => `What is ${formatName(firstName)}'s office number?`,
+        when: ({ role }) => {
+            if (role === 'Manager') {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validate: officeNumberInput => {
+            if (!isNaN(parseInt(officeNumer))) {
+                return true;
+            } else {
+                console.log('Please enter a valid office number!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: ({ firstName }) => `What is ${formatName(firstName)}'s github? username?`,
+        when: ({ role }) => {
+            if (role === 'Engineer') {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter a username!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input', 
+        name: 'school',
+        message: ({ firstName }) => `What is ${formatName(firstName)}'s school name?`,
+        when: ({ role }) => {
+            if (role === 'Intern') {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validate: schoolInput => {
+            if (schoolInput) {
+                return true;
+            } else {
+                console.log('Please enter a school name!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'newEmployee',
+        message: 'Add a new employee profile?',
+        default: true
     }
 ]
+
+const promptUser = () => {
+    return inquirer.prompt(questions)
+    .then(userResponse => {
+        employees.push(userResponse);
+        if (userResponse.newEmployee) {
+            return promptUser();
+        } else {
+            return employees;
+        };
+    });
+};
+
+const writePage = (html) => {
+    fs.writeFile('./dist/index.html', html, err => {
+        if (err) {
+            throw err
+        };
+        console.log('HTML Page Created!');
+    });
+};
+
+console.log(`Welcome to your Team Profile Generator. Please add some employees!`);
+
+promptUser()
+    .then(data => generatePage(data))
+    .then(finishedHtml => writePage(finishedHtml))
+    .catch(err => console.log(err));
