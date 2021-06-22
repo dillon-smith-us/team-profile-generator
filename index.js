14b6b8827df2e381d57ff261f494e847e9122fcb
Dillon Smith <dillsmth@gmail.com>
2021-06-22 15:49:09 -0500

    new commits

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
 const generateHTML = require('./src/generateHTML');
 const fs = require('fs');
-
-const Manager = require('./lib/Manager')
-const Engineer = require('./lib/Engineer')
-const Intern = require('./lib/Intern')
+const inquirer = require('inquirer')
+const generatePage = Require('./src/page-template.js')
+const formatName = require('./utils/helper');
+const employees = [];
+const questions = [
+    {
+        type: 'list',
+        name: 'role',
+        message: 'what is the employee\'s role?',
+        choices: () => {
+            if (allEmployees.some(employee => employee.role === 'Manager')) {
+                return['Engineer', 'Intern']
+            } else {
+                return['Manager', 'Engineer', 'Intern']
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'firstName',
+        message: ({role}) => `What is the ${role.toLowerCase()}'s first name?`,
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('please enter a first name!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'lastName',
+        message: ({firstName}) => `What is ${formatName(firstName)}'s last name?`,
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('please enter a last name!');
+                return false;
+            }
+        }
+    }
+]
